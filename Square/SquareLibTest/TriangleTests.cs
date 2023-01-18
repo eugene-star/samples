886477ddd6aba5bf20098ef456906838883fa5a6
eugene-star <55133576+eugene-star@users.noreply.github.com>
2023-01-18 14:02:40 +0300

    double to decimal

--- Square/SquareLibTest/TriangleTests.cs
+++ Square/SquareLibTest/TriangleTests.cs
@@ -10,63 +10,37 @@ namespace Tests
 		[Test]
 		public void TriangleTest1()
 		{
-			var triangle = new Triangle();
-			Assert.AreEqual(triangle.Square(), 0);
-		}
-
+			Assert.Throws<ArgumentException>(() => { new Triangle().SetSides(-1, 0, 0); });
+		}		
+		
 		[Test]
 		public void TriangleTest2()
 		{
-			var triangle = new Triangle() { SideA = 2, SideB = 2, SideC = 2 };
-			Assert.AreEqual(triangle.Square(), Math.Sqrt(3));
+			Assert.Throws<ArgumentException>(() => { new Triangle().SetSides(1, 1, 3); }); 
 		}
 
 		[Test]
 		public void TriangleTest3()
 		{
-			var triangle = new Triangle() { SideA = 2, SideB = 2, SideC = 2 };
-			Assert.AreEqual(triangle.IsRight(), false);
+			Assert.AreEqual((new Triangle()).Square(), 0);
 		}
 
 		[Test]
 		public void TriangleTest4()
 		{
-			var triangle = new Triangle() { SideA = 3, SideB = 4, SideC = 5 };
-			Assert.AreEqual(triangle.IsRight(), true);
+			Assert.AreEqual((new Triangle().SetSides(2, 2, 2)).Square(), Math.Sqrt(3));
 		}
 
 		[Test]
 		public void TriangleTest5()
 		{
-			var triangle = new Triangle() { SideA = -1 };
-
-			try
-			{
-				triangle.Square();
-			}
-			catch (ArgumentException)
-			{
-				Assert.Pass();
-			}
-
-			Assert.Fail();
+			Assert.IsFalse((new Triangle().SetSides(2, 2, 2)).IsRight());
 		}
 
 		[Test]
 		public void TriangleTest6()
 		{
-			var triangle = new Triangle() { SideA = 3, SideB = 4, SideC = 8 };
-
-			try
-			{
-				triangle.Square();
-			}
-			catch (ArgumentException)
-			{
-				Assert.Pass();
-			}
-
-			Assert.Fail();
+			Assert.IsTrue((new Triangle().SetSides(3, 4, 5)).IsRight());
 		}
 	}
 }
